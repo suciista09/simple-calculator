0e79456d72416c1238d3313c300d06022b7f6d76
suciista09 <suci.ij@gmail.com>
2021-08-05 15:14:38 +0700

    first commit

---
+++ src/main/java/Operator.java
@@ -0,0 +1,10 @@
+public class Operator {
+
+    public int add(int number1, int number2){
+        return number1 + number2;
+    }
+
+    public int multiply(int number1, int number2){
+        return number1 * number2;
+    }
+}
---
+++ src/test/java/OperatorTest.java
@@ -0,0 +1,21 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OperatorTest {
+
+    @Test
+    public void testAdd(){
+        Operator operator = new Operator();
+
+        int result = operator.add(5,9);
+        Assert.assertEquals(14, result);
+    }
+
+    @Test
+    public void testMultiply(){
+        Operator operator = new Operator();
+
+        int result = operator.multiply(2,2);
+        Assert.assertEquals(4, result);
+    }
+}
